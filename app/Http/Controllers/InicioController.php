65999b684145aee6e545c0c1d966942b9657dddd
martiresalbertoorozcogonzales <martiresalbertoorozcogonzales@gmail.com>
2020-10-11 14:51:13 -0600

    Actualizacion 11/10/2020 Obtener y mostrar recetas desde su categoria y sus likes

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Receta;
+use App\CategoriaReceta;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class InicioController extends Controller
@@ -10,9 +12,24 @@ class InicioController extends Controller
     public function index()
     {
 
-        //Obtener las recetas nuevas
-        $nuevas = Receta::latest()->take(5)->get();
+        //Mostrar recetas por cantidad de votos  
+        //$votadas = Receta::has('likes', '>', 0)->get();
+        $votadas = Receta::withCount('likes')->orderBy('likes_count','desc')->take(3)->get();
 
-        return view('inicio.index',compact('nuevas'));
+        //Obtener las recetas nuevas
+        $nuevas = Receta::latest()->take(6)->get();
+
+        // Obterner las recetas por categoria
+        $categorias = CategoriaReceta::all();
+
+        //Agragr las recetas a categoria
+        $recetas = [];
+        foreach ($categorias as $categoria) {
+            $recetas[ Str::slug( $categoria->nombre )][] = Receta::where('categoria_id', $categoria->id)->take(3)->get();
+        }
+
+        // return $recetas; 
+
+        return view('inicio.index',compact('nuevas','recetas','votadas'));
     }
 }
